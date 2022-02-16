63a9f2b01ba0352ad1947f8c22fabb38426d97d7
Seqam Lab <49203787+seqam-lab@users.noreply.github.com>
2022-02-16 10:39:52 -0500

    apply_poe bug fixed

--- util.py
+++ util.py
@@ -4,6 +4,53 @@ import torch.nn.init as init
 import random
 import numpy as np
 
+def logsumexp(x, dim=None, keepdim=False):
+    if dim is None:
+        x, dim = x.view(-1), 0
+    xm, _ = torch.max(x, dim, keepdim=True)
+    x = torch.where(
+        (xm == float('inf')) | (xm == float('-inf')),
+        xm,
+        xm + torch.log(torch.sum(torch.exp(x - xm), dim, keepdim=True)))
+    return x if keepdim else x.squeeze(dim)
+
+
+def apply_poe(use_cuda, mu_sharedA, std_sharedA, mu_sharedB=None, std_sharedB=None):
+    '''
+    induce zS = encAB(xA,xB) via POE, that is,
+        q(zI,zT,zS|xI,xT) := qI(zI|xI) * qT(zT|xT) * q(zS|xI,xT)
+            where q(zS|xI,xT) \propto p(zS) * qI(zS|xI) * qT(zS|xT)
+    '''
+    EPS = 1e-9
+    ZERO = torch.zeros(std_sharedA.shape)
+    if use_cuda:
+        ZERO = ZERO.cuda()
+
+    logvar_sharedA = torch.log(std_sharedA ** 2 + EPS)
+
+    if mu_sharedB is not None and std_sharedB is not None:
+        logvar_sharedB = torch.log(std_sharedB ** 2 + EPS)
+        logvarS = -logsumexp(
+            torch.stack((ZERO, -logvar_sharedA, -logvar_sharedB), dim=2),
+            dim=2
+        )
+    else:
+        logvarS = -logsumexp(
+            torch.stack((ZERO, -logvar_sharedA), dim=2),
+            dim=2
+        )
+    stdS = torch.sqrt(torch.exp(logvarS))
+
+    if mu_sharedB is not None and std_sharedB is not None:
+        muS = (mu_sharedA / (std_sharedA ** 2 + EPS) +
+               mu_sharedB / (std_sharedB ** 2 + EPS)) * (stdS ** 2)
+    else:
+        muS = (mu_sharedA / (std_sharedA ** 2 + EPS)) * (stdS ** 2)
+
+    return muS, stdS
+
+
+
 def kaiming_init(m, seed):
     random.seed(seed)
     np.random.seed(seed)
